76dfbcf689475cb47aea09352b01b9dcdd4b0795
Brady <braden.jr.mitch@gmail.com>
2022-03-16 19:18:32 -0700

    End Lab 7

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,15 +6,34 @@ public class SceneController : MonoBehaviour
 {
 
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject iguanaPrefab;
+
     private Vector3 spawnPoint = new Vector3(0, 0, 5);
+    private Vector3 spawnPointIguana = new Vector3(0, 2.384186f, 18);
 
     // multiple enemies
     private int numEnemies = 5;
     private GameObject[] enemies;
 
+    private int numIguanas = 7;
+    private GameObject[] iguanas;
+
     private void Start()
     {
         enemies = new GameObject[numEnemies];
+        iguanas = new GameObject[numIguanas];
+
+        for (int i = 0; i < iguanas.Length; i++)
+        {
+            GameObject spawnableIguana;
+
+            spawnableIguana = Instantiate(iguanaPrefab) as GameObject;
+            spawnableIguana.transform.position = spawnPointIguana;
+            float angle = Random.Range(0, 360);
+            spawnableIguana.transform.Rotate(0, angle, 0);
+
+            iguanas[i] = spawnableIguana;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -7,7 +7,7 @@ public enum EnemyStates { alive, dead };
 public class WanderingAI : MonoBehaviour
 {
 
-    private float enemySpeed = 3.0f;
+    private float enemySpeed = 1.75f;
     private float obstacleRange = 5.0f;
     private float sphereRadius = 1f;
 
